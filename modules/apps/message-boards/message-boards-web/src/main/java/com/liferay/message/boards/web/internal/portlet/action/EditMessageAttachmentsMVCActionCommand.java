d7efe4ea19127e791625557d5794cadb44c0102b
Julius Lee <meng-huan.lee@liferay.com>
2022-01-07 15:44:58 -0800

    LPS-143631 Remove JSONFactoryUtil usages in modules, part 5

--- modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/portlet/action/EditMessageAttachmentsMVCActionCommand.java
+++ modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/portlet/action/EditMessageAttachmentsMVCActionCommand.java
@@ -19,7 +19,7 @@ import com.liferay.message.boards.constants.MBPortletKeys;
 import com.liferay.message.boards.service.MBMessageService;
 import com.liferay.message.boards.web.internal.upload.TempAttachmentMBUploadFileEntryHandler;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -96,7 +96,7 @@ public class EditMessageAttachmentsMVCActionCommand
 		long categoryId = ParamUtil.getLong(uploadPortletRequest, "categoryId");
 		String fileName = ParamUtil.getString(actionRequest, "fileName");
 
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+		JSONObject jsonObject = _jsonFactory.createJSONObject();
 
 		try {
 			_mbMessageService.deleteTempAttachment(
@@ -183,6 +183,9 @@ public class EditMessageAttachmentsMVCActionCommand
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		EditMessageAttachmentsMVCActionCommand.class);
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 	@Reference
 	private MBMessageService _mbMessageService;
