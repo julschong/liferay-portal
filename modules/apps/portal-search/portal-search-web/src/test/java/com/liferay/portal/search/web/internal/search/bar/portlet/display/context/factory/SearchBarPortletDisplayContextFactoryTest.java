8c3d7b5ddeb274522ddd884a8d252d897ab8d3ce
Julius Lee <meng-huan.lee@liferay.com>
2023-03-01 11:54:29 -0800

    LPS-173369 Apply to usages

--- modules/apps/portal-search/portal-search-web/src/test/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactoryTest.java
+++ modules/apps/portal-search/portal-search-web/src/test/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactoryTest.java
@@ -44,8 +44,6 @@ import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchRe
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.portlet.PortletPreferencesImpl;
 
-import java.util.Optional;
-
 import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
 import javax.portlet.ReadOnlyException;
@@ -478,7 +476,7 @@ public class SearchBarPortletDisplayContextFactoryTest {
 			portletSharedSearchResponse.getParameter(
 				Mockito.eq(scopeParameterName), Mockito.any())
 		).thenReturn(
-			Optional.ofNullable(scopeParameterValue)
+			scopeParameterValue
 		);
 
 		SearchResponse searchResponse = Mockito.mock(SearchResponse.class);
