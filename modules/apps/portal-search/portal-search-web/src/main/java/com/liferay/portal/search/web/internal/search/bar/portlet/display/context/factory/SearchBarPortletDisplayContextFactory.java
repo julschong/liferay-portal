2a69df247a1445419997f59fe84ef6c31a734f9f
Julius Lee <meng-huan.lee@liferay.com>
2023-03-13 17:29:22 -0700

    LPS-178022 Apply to usages

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactory.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactory.java
@@ -309,13 +309,13 @@ public class SearchBarPortletDisplayContextFactory {
 			return searchBarPortletPreferences.getSearchScopePreference();
 		}
 
-		Optional<PortletPreferences> headerPortletPreferencesOptional =
+		PortletPreferences portletPreferences =
 			portletPreferencesLookup.fetchPreferences(
 				headerSearchBarPortlet, themeDisplay);
 
-		if (!headerPortletPreferencesOptional.isPresent() ||
+		if ((portletPreferences == null) ||
 			!SearchBarPortletDestinationUtil.isSameDestination(
-				headerPortletPreferencesOptional.get(), themeDisplay)) {
+				portletPreferences, themeDisplay)) {
 
 			return searchBarPortletPreferences.getSearchScopePreference();
 		}
@@ -372,13 +372,13 @@ public class SearchBarPortletDisplayContextFactory {
 			return searchBarPortletPreferences.getKeywordsParameterName();
 		}
 
-		Optional<PortletPreferences> headerPortletPreferencesOptional =
+		PortletPreferences portletPreferences =
 			portletPreferencesLookup.fetchPreferences(
 				headerSearchBarPortlet, themeDisplay);
 
-		if (!headerPortletPreferencesOptional.isPresent() ||
+		if ((portletPreferences == null) ||
 			!SearchBarPortletDestinationUtil.isSameDestination(
-				headerPortletPreferencesOptional.get(), themeDisplay)) {
+				portletPreferences, themeDisplay)) {
 
 			return searchBarPortletPreferences.getKeywordsParameterName();
 		}
@@ -402,13 +402,13 @@ public class SearchBarPortletDisplayContextFactory {
 			return searchBarPortletPreferences.getScopeParameterName();
 		}
 
-		Optional<PortletPreferences> headerPortletPreferencesOptional =
+		PortletPreferences portletPreferences =
 			portletPreferencesLookup.fetchPreferences(
 				headerSearchBarPortlet, themeDisplay);
 
-		if (!headerPortletPreferencesOptional.isPresent() ||
+		if ((portletPreferences == null) ||
 			!SearchBarPortletDestinationUtil.isSameDestination(
-				headerPortletPreferencesOptional.get(), themeDisplay)) {
+				portletPreferences, themeDisplay)) {
 
 			return searchBarPortletPreferences.getScopeParameterName();
 		}
