f37433b326079fd9ac4eae85da501242dd6399e4
Julius Lee <meng-huan.lee@liferay.com>
2023-03-13 18:08:40 -0700

    LPS-178022 Remove Stream/Optional usages in SearchResultSummaryDisplayContextBuilder

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortlet.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortlet.java
@@ -388,8 +388,8 @@ public class SearchResultsPortlet extends MVCPortlet {
 			getHttpServletRequest(renderRequest)
 		).setResourceActions(
 			resourceActions
-		).setSearchResultImageContributorsStream(
-			_searchResultImageContributors.stream()
+		).setSearchResultImageContributors(
+			_searchResultImageContributors
 		).setSearchResultPreferences(
 			searchResultPreferences
 		).setSummaryBuilderFactory(
