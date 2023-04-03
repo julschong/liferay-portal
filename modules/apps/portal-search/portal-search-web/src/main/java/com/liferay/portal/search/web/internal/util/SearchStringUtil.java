d42f911f8d23a4d26f3e411ff3da5d3b0758f586
Julius Lee <meng-huan.lee@liferay.com>
2023-04-03 14:30:21 -0700

    LPS-178022 Remove Stream usage in SearchStringUtil

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/util/SearchStringUtil.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/util/SearchStringUtil.java
@@ -14,11 +14,10 @@
 
 package com.liferay.portal.search.web.internal.util;
 
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
-
-import java.util.stream.Stream;
 
 /**
  * @author André de Oliveira
@@ -36,15 +35,9 @@ public class SearchStringUtil {
 	}
 
 	public static String[] splitAndUnquote(String s) {
-		return Stream.of(
-			StringUtil.split(s.trim(), CharPool.COMMA)
-		).map(
-			String::trim
-		).map(
-			StringUtil::unquote
-		).toArray(
-			String[]::new
-		);
+		return TransformUtil.transform(
+			StringUtil.split(s.trim(), CharPool.COMMA),
+			part -> StringUtil.unquote(part.trim()), String.class);
 	}
 
 }
