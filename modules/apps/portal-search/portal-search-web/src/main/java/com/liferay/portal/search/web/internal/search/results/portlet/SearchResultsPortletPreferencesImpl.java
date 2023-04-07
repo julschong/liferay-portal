0761385ecd0debd1294681d378c82bb4b7948229
Julius Lee <meng-huan.lee@liferay.com>
2023-04-07 16:48:43 -0700

    LPS-178022 Remove Optional from SearchResult

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortletPreferencesImpl.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortletPreferencesImpl.java
@@ -45,14 +45,10 @@ public class SearchResultsPortletPreferencesImpl
 	}
 
 	@Override
-	public Optional<String> getFieldsToDisplayOptional() {
-		return _portletPreferencesHelper.getString(
-			SearchResultsPortletPreferences.PREFERENCE_KEY_FIELDS_TO_DISPLAY);
-	}
-
-	@Override
 	public String getFieldsToDisplayString() {
-		return getFieldsToDisplayOptional().orElse(StringPool.BLANK);
+		return _portletPreferencesHelper.getString(
+			SearchResultsPortletPreferences.PREFERENCE_KEY_FIELDS_TO_DISPLAY,
+			StringPool.BLANK);
 	}
 
 	@Override
