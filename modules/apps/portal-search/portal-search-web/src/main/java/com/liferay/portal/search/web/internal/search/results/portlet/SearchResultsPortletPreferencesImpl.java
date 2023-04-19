2a6055916df638da2e21ccc1ee2bb54f618e7bff
Julius Lee <meng-huan.lee@liferay.com>
2023-04-19 16:20:03 -0700

    LPS-178022 Remove getValueOptional method in SearchResult, no more usages

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortletPreferencesImpl.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortletPreferencesImpl.java
@@ -45,14 +45,11 @@ public class SearchResultsPortletPreferencesImpl
 	}
 
 	@Override
-	public Optional<String> getFieldsToDisplayOptional() {
-		return _portletPreferencesHelper.getString(
-			SearchResultsPortletPreferences.PREFERENCE_KEY_FIELDS_TO_DISPLAY);
-	}
-
-	@Override
 	public String getFieldsToDisplayString() {
-		return getFieldsToDisplayOptional().orElse(StringPool.BLANK);
+		return GetterUtil.getString(
+			_portletPreferencesHelper.getString(
+				SearchResultsPortletPreferences.
+					PREFERENCE_KEY_FIELDS_TO_DISPLAY));
 	}
 
 	@Override
