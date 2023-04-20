4adde88b38f0ac50ff01cf7b8587fe8d5dc6dcb2
Julius Lee <meng-huan.lee@liferay.com>
2023-04-20 13:57:13 -0700

    LPS-178022 Decouple from PorletPreferencesHelper, use PortletPreferences directly instead and assert that it has to be present

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortletPreferencesImpl.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortletPreferencesImpl.java
@@ -16,9 +16,6 @@ package com.liferay.portal.search.web.internal.search.results.portlet;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.search.web.internal.helper.PortletPreferencesHelper;
-import com.liferay.portal.util.PropsUtil;
 
 import java.util.Optional;
 
@@ -33,37 +30,37 @@ public class SearchResultsPortletPreferencesImpl
 	public SearchResultsPortletPreferencesImpl(
 		Optional<PortletPreferences> portletPreferencesOptional) {
 
-		_portletPreferencesHelper = new PortletPreferencesHelper(
-			portletPreferencesOptional);
+		_portletPreferences = portletPreferencesOptional.orElseThrow(
+			() -> new IllegalArgumentException(
+				"PortletPreferences is not present"));
 	}
 
 	@Override
 	public String getFederatedSearchKey() {
-		return _portletPreferencesHelper.getString(
+		return _portletPreferences.getValue(
 			SearchResultsPortletPreferences.PREFERENCE_KEY_FEDERATED_SEARCH_KEY,
 			StringPool.BLANK);
 	}
 
 	@Override
 	public String getFieldsToDisplay() {
-		Optional<String> optional = _portletPreferencesHelper.getString(
-			SearchResultsPortletPreferences.PREFERENCE_KEY_FIELDS_TO_DISPLAY);
-
-		return optional.orElse(StringPool.BLANK);
+		return _portletPreferences.getValue(
+			SearchResultsPortletPreferences.PREFERENCE_KEY_FIELDS_TO_DISPLAY,
+			StringPool.BLANK);
 	}
 
 	@Override
 	public int getPaginationDelta() {
-		return _portletPreferencesHelper.getInteger(
-			SearchResultsPortletPreferences.PREFERENCE_KEY_PAGINATION_DELTA,
-			GetterUtil.getInteger(
-				PropsUtil.get(PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA),
-				20));
+		return GetterUtil.getInteger(
+			_portletPreferences.getValue(
+				SearchResultsPortletPreferences.PREFERENCE_KEY_PAGINATION_DELTA,
+				StringPool.BLANK),
+			20);
 	}
 
 	@Override
 	public String getPaginationDeltaParameterName() {
-		return _portletPreferencesHelper.getString(
+		return _portletPreferences.getValue(
 			SearchResultsPortletPreferences.
 				PREFERENCE_KEY_PAGINATION_DELTA_PARAMETER_NAME,
 			"delta");
@@ -71,7 +68,7 @@ public class SearchResultsPortletPreferencesImpl
 
 	@Override
 	public String getPaginationStartParameterName() {
-		return _portletPreferencesHelper.getString(
+		return _portletPreferences.getValue(
 			SearchResultsPortletPreferences.
 				PREFERENCE_KEY_PAGINATION_START_PARAMETER_NAME,
 			"start");
@@ -79,26 +76,32 @@ public class SearchResultsPortletPreferencesImpl
 
 	@Override
 	public boolean isDisplayInDocumentForm() {
-		return _portletPreferencesHelper.getBoolean(
-			SearchResultsPortletPreferences.
-				PREFERENCE_KEY_DISPLAY_IN_DOCUMENT_FORM,
-			false);
+		return GetterUtil.getBoolean(
+			_portletPreferences.getValue(
+				SearchResultsPortletPreferences.
+					PREFERENCE_KEY_DISPLAY_IN_DOCUMENT_FORM,
+				StringPool.BLANK));
 	}
 
 	@Override
 	public boolean isHighlightEnabled() {
-		return _portletPreferencesHelper.getBoolean(
-			SearchResultsPortletPreferences.PREFERENCE_KEY_HIGHLIGHT_ENABLED,
+		return GetterUtil.getBoolean(
+			_portletPreferences.getValue(
+				SearchResultsPortletPreferences.
+					PREFERENCE_KEY_HIGHLIGHT_ENABLED,
+				StringPool.BLANK),
 			true);
 	}
 
 	@Override
 	public boolean isViewInContext() {
-		return _portletPreferencesHelper.getBoolean(
-			SearchResultsPortletPreferences.PREFERENCE_KEY_VIEW_IN_CONTEXT,
+		return GetterUtil.getBoolean(
+			_portletPreferences.getValue(
+				SearchResultsPortletPreferences.PREFERENCE_KEY_VIEW_IN_CONTEXT,
+				StringPool.BLANK),
 			true);
 	}
 
-	private final PortletPreferencesHelper _portletPreferencesHelper;
+	private final PortletPreferences _portletPreferences;
 
 }
