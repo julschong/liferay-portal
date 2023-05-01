ff40f918e4d39e83c3bacbc7fdde6baaf60bb616
Julius Lee <meng-huan.lee@liferay.com>
2023-05-01 10:26:38 -0700

    LPS-178022 Remove Optional from PortletSharedSearchResponse.getParameter()

--- modules/apps/portal-search/portal-search-web-api/src/main/java/com/liferay/portal/search/web/portlet/shared/search/PortletSharedSearchResponse.java
+++ modules/apps/portal-search/portal-search-web-api/src/main/java/com/liferay/portal/search/web/portlet/shared/search/PortletSharedSearchResponse.java
@@ -46,7 +46,7 @@ public interface PortletSharedSearchResponse {
 
 	public int getPaginationStart();
 
-	public Optional<String> getParameter(
+	public String getParameter(
 		String name, RenderRequest renderRequest);
 
 	public Optional<String[]> getParameterValues(
