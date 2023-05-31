8c2ae98d33bfc50db8be5cce435f490f50e4e97a
Julius Lee <meng-huan.lee@liferay.com>
2023-05-30 17:27:40 -0700

    LPS-185105 Remove deprecated requireIndexReadWrite and setIndexReadOnly methods in IndexStatusManager

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/index/IndexStatusManagerImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/index/IndexStatusManagerImpl.java
@@ -14,11 +14,8 @@
 
 package com.liferay.portal.search.internal.index;
 
-import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.events.StartupHelperUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.IndexStatusManagerThreadLocal;
 import com.liferay.portal.search.configuration.IndexStatusManagerConfiguration;
 import com.liferay.portal.search.index.IndexStatusManager;
@@ -65,66 +62,6 @@ public class IndexStatusManagerImpl implements IndexStatusManager {
 		return _indexReadOnlyModels.contains(className);
 	}
 
-	@Override
-	public void requireIndexReadWrite(boolean required) {
-		if (required) {
-			if (_indexReadOnly) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						new Throwable(
-							StringBundler.concat(
-								"Setting index to Read-Write as required by ",
-								"incoming operation, overriding currently set ",
-								"Read-Only (see exception for Read-Write call ",
-								"stack and nested exception for Read-Only ",
-								"call stack)"),
-							_indexReadOnlyCallStackThrowable));
-				}
-
-				_indexReadOnly = false;
-			}
-
-			_requireIndexReadWriteCallStackThrowable = new Throwable();
-		}
-
-		_readWriteRequired = required;
-	}
-
-	@Override
-	public void setIndexReadOnly(boolean indexReadOnly) {
-		if (indexReadOnly) {
-			if (_readWriteRequired) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						new Throwable(
-							StringBundler.concat(
-								"Suppressing attempt to set index to ",
-								"Read-Only while ongoing operation requires ",
-								"Read-Write (see exception for Read-Only call ",
-								"stack and nested exception for Read-Write ",
-								"call stack)"),
-							_requireIndexReadWriteCallStackThrowable));
-				}
-
-				return;
-			}
-
-			_indexReadOnlyCallStackThrowable = new Throwable();
-		}
-
-		_indexReadOnly = indexReadOnly;
-	}
-
-	@Override
-	public void setIndexReadOnly(String className, boolean indexReadOnly) {
-		if (indexReadOnly) {
-			_indexReadOnlyModels.add(className);
-		}
-		else {
-			_indexReadOnlyModels.remove(className);
-		}
-	}
-
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
@@ -143,15 +80,9 @@ public class IndexStatusManagerImpl implements IndexStatusManager {
 			indexStatusManagerInternalConfiguration.suppressIndexReadOnly();
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		IndexStatusManagerImpl.class);
-
 	private volatile boolean _indexReadOnly;
-	private Throwable _indexReadOnlyCallStackThrowable;
 	private final Set<String> _indexReadOnlyModels = Collections.newSetFromMap(
 		new ConcurrentHashMap<>());
-	private boolean _readWriteRequired;
-	private Throwable _requireIndexReadWriteCallStackThrowable;
 	private volatile boolean _suppressIndexReadOnly;
 
 }
