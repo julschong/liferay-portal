ed7f7c553a3a3e860339a209584102a42254ff1c
Julius Lee <meng-huan.lee@liferay.com>
2023-06-13 16:57:48 -0700

    LPS-185768 Add upgrade process to remove AMImageConfiguration.imageMaxSize

--- modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/upgrade/v1_0_0/AMImageConfigurationUpgradeProcess.java
+++ modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/upgrade/v1_0_0/AMImageConfigurationUpgradeProcess.java
@@ -15,13 +15,20 @@
 package com.liferay.adaptive.media.image.internal.upgrade.v1_0_0;
 
 import com.liferay.adaptive.media.image.internal.configuration.AMImageConfiguration;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.ArrayUtil;
 
 import java.io.IOException;
 
+import java.lang.reflect.Method;
+
+import java.sql.SQLException;
+
 import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.osgi.framework.Constants;
 import org.osgi.framework.InvalidSyntaxException;
@@ -40,7 +47,9 @@ public class AMImageConfigurationUpgradeProcess extends UpgradeProcess {
 	}
 
 	@Override
-	protected void doUpgrade() throws InvalidSyntaxException, IOException {
+	protected void doUpgrade()
+		throws InvalidSyntaxException, IOException, SQLException {
+
 		Configuration[] configurations = _configurationAdmin.listConfigurations(
 			StringBundler.concat(
 				"(", Constants.SERVICE_PID, "=",
@@ -50,15 +59,29 @@ public class AMImageConfigurationUpgradeProcess extends UpgradeProcess {
 			return;
 		}
 
+		Configuration configuration = configurations[0];
 
-		for (Configuration configuration : configurations) {
-			Dictionary<String, Object> dictionary =
-				configuration.getProperties();
+		Dictionary<String, Object> dictionary = configuration.getProperties();
 
-			dictionary.remove("imageMaxSize");
+		dictionary.remove("imageMaxSize");
 
-			configuration.updateIfDifferent(dictionary);
+		configuration.updateIfDifferent(dictionary);
+
+		Set<String> properties = new HashSet<>(
+			TransformUtil.transformToList(
+				AMImageConfiguration.class.getMethods(), Method::getName));
+
+		Enumeration<String> enumeration = dictionary.keys();
+
+		while (enumeration.hasMoreElements()) {
+			String key = enumeration.nextElement();
+
+			if (properties.contains(key)) {
+				return;
+			}
 		}
+
+		configuration.delete();
 	}
 
 	private final ConfigurationAdmin _configurationAdmin;
