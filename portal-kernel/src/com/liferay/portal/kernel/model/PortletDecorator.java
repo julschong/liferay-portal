f68463ba96713f25f867789610f05fe416405905
Julius Lee <meng-huan.lee@liferay.com>
2023-08-09 18:26:46 -0700

    LPS-XXXXXX Change PortletDecorator from interface into class and merge impl logic into kernel

--- portal-kernel/src/com/liferay/portal/kernel/model/PortletDecorator.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/PortletDecorator.java
@@ -5,15 +5,18 @@
 
 package com.liferay.portal.kernel.model;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.kernel.util.Accessor;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.io.Serializable;
 
 /**
  * @author Eduardo García
  */
-public interface PortletDecorator
-	extends Comparable<PortletDecorator>, Serializable {
+public class PortletDecorator
+	implements Comparable<PortletDecorator>, Serializable {
 
 	public static final Accessor<PortletDecorator, String> NAME_ACCESSOR =
 		new Accessor<PortletDecorator, String>() {
@@ -35,23 +38,116 @@ public interface PortletDecorator
 
 		};
 
-	public String getCssClass();
+	public PortletDecorator() {
+		this(null, null, null);
+	}
 
-	public String getName();
+	public PortletDecorator(String portletDecoratorId) {
+		this(portletDecoratorId, null, null);
+	}
 
-	public String getPortletDecoratorId();
+	public PortletDecorator(
+		String portletDecoratorId, String name, String cssClass) {
 
-	public String getPortletDecoratorThumbnailPath();
+		_portletDecoratorId = portletDecoratorId;
+		_name = name;
+		_cssClass = cssClass;
+	}
 
-	public boolean isDefaultPortletDecorator();
+	@Override
+	public int compareTo(PortletDecorator portletDecorator) {
+		return getName().compareTo(portletDecorator.getName());
+	}
 
-	public void setCssClass(String cssClass);
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
 
-	public void setDefaultPortletDecorator(boolean defaultPortletDecorator);
+		if (!(object instanceof PortletDecorator)) {
+			return false;
+		}
 
-	public void setName(String name);
+		PortletDecorator portletDecorator = (PortletDecorator)object;
+
+		if (getPortletDecoratorId().equals(
+				portletDecorator.getPortletDecoratorId())) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public String getCssClass() {
+		return _cssClass;
+	}
+
+	public String getName() {
+		if (Validator.isNull(_name)) {
+			return _portletDecoratorId;
+		}
+
+		return _name;
+	}
+
+	public String getPortletDecoratorId() {
+		return _portletDecoratorId;
+	}
+
+	public String getPortletDecoratorThumbnailPath() {
+		if (Validator.isNotNull(_cssClass) &&
+			Validator.isNotNull(_portletDecoratorThumbnailPath)) {
+
+			int pos = _cssClass.indexOf(CharPool.SPACE);
+
+			if ((pos > 0) &&
+				_portletDecoratorThumbnailPath.endsWith(
+					_cssClass.substring(0, pos))) {
+
+				String subclassPath = StringUtil.replace(
+					_cssClass, CharPool.SPACE, CharPool.SLASH);
+
+				return _portletDecoratorThumbnailPath +
+					subclassPath.substring(pos);
+			}
+		}
+
+		return _portletDecoratorThumbnailPath;
+	}
+
+	public int hashCode() {
+		return _portletDecoratorId.hashCode();
+	}
+
+	public boolean isDefaultPortletDecorator() {
+		return _defaultPortletDecorator;
+	}
+
+	public void setCssClass(String cssClass) {
+		_cssClass = cssClass;
+	}
+
+	public void setDefaultPortletDecorator(boolean defaultPortletDecorator) {
+		_defaultPortletDecorator = defaultPortletDecorator;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
 
 	public void setPortletDecoratorThumbnailPath(
-		String portletDecoratorThumbnailPath);
+		String portletDecoratorThumbnailPath) {
+
+		_portletDecoratorThumbnailPath = portletDecoratorThumbnailPath;
+	}
+
+	private String _cssClass;
+	private boolean _defaultPortletDecorator;
+	private String _name;
+	private final String _portletDecoratorId;
+	private String _portletDecoratorThumbnailPath =
+		"${images-path}/portlet_decorators/${portlet-decorator-css-class}";
 
 }
