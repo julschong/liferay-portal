de03232f4f43baab30db9a2d06e59b60c086dad6
Julius Lee <meng-huan.lee@liferay.com>
2023-08-15 14:39:20 -0700

    LPS-193396 Move ImageMagickUtil to server-admin-web for only usage in external_services.jsp

--- modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/image/ImageMagickUtil.java
+++ modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/image/ImageMagickUtil.java
@@ -3,8 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.image;
+package com.liferay.server.admin.web.internal.image;
 
+import com.liferay.portal.kernel.image.ImageMagick;
 import com.liferay.portal.kernel.util.ServiceProxyFactory;
 
 import java.util.List;
