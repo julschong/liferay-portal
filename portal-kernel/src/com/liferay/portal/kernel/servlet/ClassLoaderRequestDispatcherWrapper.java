416235ef4c3521cd06f4dd2a7307a29902a80067
Julius Lee <meng-huan.lee@liferay.com>
2023-09-07 15:34:17 -0700

    LPS-195582 Move RequestDispatcher classes into kernel prepping next

--- portal-kernel/src/com/liferay/portal/kernel/servlet/ClassLoaderRequestDispatcherWrapper.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/ClassLoaderRequestDispatcherWrapper.java
@@ -3,9 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.servlet;
+package com.liferay.portal.kernel.servlet;
 
-import com.liferay.portal.kernel.servlet.PluginContextListener;
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 
 import java.io.IOException;
--- portal-kernel/src/com/liferay/portal/kernel/servlet/DirectRequestDispatcher.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/DirectRequestDispatcher.java
@@ -3,9 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.servlet;
-
-import com.liferay.portal.kernel.servlet.DynamicServletRequest;
+package com.liferay.portal.kernel.servlet;
 
 import java.io.IOException;
 
