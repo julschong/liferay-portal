5c99cbd7d1ffc1a0634eafcaf531a6b61b30107c
Julius Lee <meng-huan.lee@liferay.com>
2023-09-12 11:31:02 -0700

    LPS-195954 Move Generator implementations to kernel

--- portal-kernel/src/com/liferay/portal/kernel/cache/key/MessageDigestCacheKeyGenerator.java
+++ portal-kernel/src/com/liferay/portal/kernel/cache/key/MessageDigestCacheKeyGenerator.java
@@ -3,12 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.cache.key;
+package com.liferay.portal.kernel.cache.key;
 
 import com.liferay.petra.nio.CharsetEncoderUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.cache.key.CacheKeyGenerator;
 import com.liferay.portal.kernel.cache.thread.local.Lifecycle;
 import com.liferay.portal.kernel.cache.thread.local.ThreadLocalCache;
 import com.liferay.portal.kernel.cache.thread.local.ThreadLocalCacheManager;
