bc01a8b3a6124504582d7a1ee582f46f9016e021
Julius Lee <meng-huan.lee@liferay.com>
2023-09-26 16:05:29 -0700

    LPS-197375 Inline DefaultModelResourcePermission into ModelResourcePermissionFactory

--- portal-kernel/src/com/liferay/portal/kernel/security/permission/resource/ModelResourcePermissionFactory.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/permission/resource/ModelResourcePermissionFactory.java
@@ -7,15 +7,18 @@ package com.liferay.portal.kernel.security.permission.resource;
 
 import com.liferay.petra.function.UnsafeFunction;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.internal.security.permission.resource.DefaultModelResourcePermission;
+import com.liferay.portal.kernel.internal.security.permission.resource.PermissionCacheKey;
 import com.liferay.portal.kernel.model.ClassedModel;
 import com.liferay.portal.kernel.model.GroupedModel;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.resource.definition.ModelResourcePermissionDefinition;
 import com.liferay.portal.kernel.service.BaseService;
 import com.liferay.portal.kernel.util.ServiceProxyFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.ToLongFunction;
@@ -87,6 +90,152 @@ public class ModelResourcePermissionFactory {
 		public void configureModelResourcePermissionLogics(
 			ModelResourcePermission<T> modelResourcePermission,
 			Consumer<ModelResourcePermissionLogic<T>> consumer);
+
+	}
+
+	private static class DefaultModelResourcePermission<T extends GroupedModel>
+		implements ModelResourcePermission<T> {
+
+		public DefaultModelResourcePermission(
+			ModelResourcePermissionDefinition<T>
+				modelResourcePermissionDefinition,
+			List<ModelResourcePermissionLogic<T>>
+				modelResourcePermissionLogics) {
+
+			_modelResourcePermissionDefinition =
+				modelResourcePermissionDefinition;
+			_modelResourcePermissionLogics = modelResourcePermissionLogics;
+
+			Class<T> modelClass =
+				modelResourcePermissionDefinition.getModelClass();
+
+			_modelName = modelClass.getName();
+		}
+
+		@Override
+		public void check(
+				PermissionChecker permissionChecker, long primaryKey,
+				String actionId)
+			throws PortalException {
+
+			if (!contains(permissionChecker, primaryKey, actionId)) {
+				throw new PrincipalException.MustHavePermission(
+					permissionChecker, _modelName, primaryKey, actionId);
+			}
+		}
+
+		@Override
+		public void check(
+				PermissionChecker permissionChecker, T model, String actionId)
+			throws PortalException {
+
+			if (!contains(permissionChecker, model, actionId)) {
+				throw new PrincipalException.MustHavePermission(
+					permissionChecker, _modelName,
+					_modelResourcePermissionDefinition.getPrimaryKey(model),
+					actionId);
+			}
+		}
+
+		@Override
+		public boolean contains(
+				PermissionChecker permissionChecker, long primaryKey,
+				String actionId)
+			throws PortalException {
+
+			Map<Object, Object> permissionChecksMap =
+				permissionChecker.getPermissionChecksMap();
+
+			PermissionCacheKey permissionCacheKey = new PermissionCacheKey(
+				_modelName, primaryKey, actionId);
+
+			Boolean contains = (Boolean)permissionChecksMap.get(
+				permissionCacheKey);
+
+			if (contains == null) {
+				contains = _contains(
+					permissionChecker,
+					_modelResourcePermissionDefinition.getModel(primaryKey),
+					actionId);
+
+				permissionChecksMap.put(permissionCacheKey, contains);
+			}
+
+			return contains;
+		}
+
+		@Override
+		public boolean contains(
+				PermissionChecker permissionChecker, T model, String actionId)
+			throws PortalException {
+
+			Map<Object, Object> permissionChecksMap =
+				permissionChecker.getPermissionChecksMap();
+
+			PermissionCacheKey permissionCacheKey = new PermissionCacheKey(
+				_modelName,
+				_modelResourcePermissionDefinition.getPrimaryKey(model),
+				actionId);
+
+			Boolean contains = (Boolean)permissionChecksMap.get(
+				permissionCacheKey);
+
+			if (contains == null) {
+				contains = _contains(permissionChecker, model, actionId);
+
+				permissionChecksMap.put(permissionCacheKey, contains);
+			}
+
+			return contains;
+		}
+
+		@Override
+		public String getModelName() {
+			return _modelName;
+		}
+
+		@Override
+		public PortletResourcePermission getPortletResourcePermission() {
+			return _modelResourcePermissionDefinition.
+				getPortletResourcePermission();
+		}
+
+		private boolean _contains(
+				PermissionChecker permissionChecker, T model, String actionId)
+			throws PortalException {
+
+			actionId = _modelResourcePermissionDefinition.mapActionId(actionId);
+
+			for (ModelResourcePermissionLogic<T> modelResourcePermissionLogic :
+					_modelResourcePermissionLogics) {
+
+				Boolean contains = modelResourcePermissionLogic.contains(
+					permissionChecker, _modelName, model, actionId);
+
+				if (contains != null) {
+					return contains;
+				}
+			}
+
+			String primKey = String.valueOf(
+				_modelResourcePermissionDefinition.getPrimaryKey(model));
+
+			if (permissionChecker.hasOwnerPermission(
+					model.getCompanyId(), _modelName, primKey,
+					model.getUserId(), actionId)) {
+
+				return true;
+			}
+
+			return permissionChecker.hasPermission(
+				model.getGroupId(), _modelName, primKey, actionId);
+		}
+
+		private final String _modelName;
+		private final ModelResourcePermissionDefinition<T>
+			_modelResourcePermissionDefinition;
+		private final List<ModelResourcePermissionLogic<T>>
+			_modelResourcePermissionLogics;
 
 	}
 
