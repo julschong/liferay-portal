c578a5755dfece91af05c8e66f71aca25444a2ef
Julius Lee <meng-huan.lee@liferay.com>
2023-10-04 17:53:41 -0700

    LPS-198155 Move UpgradeTableFactoryUtil into portal-impl

--- portal-impl/src/com/liferay/portal/upgrade/util/UpgradeTableFactoryUtil.java
+++ portal-impl/src/com/liferay/portal/upgrade/util/UpgradeTableFactoryUtil.java
@@ -3,7 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.upgrade.util;
+package com.liferay.portal.upgrade.util;
+
+import com.liferay.portal.kernel.upgrade.util.UpgradeColumn;
+import com.liferay.portal.kernel.upgrade.util.UpgradeTable;
+import com.liferay.portal.kernel.upgrade.util.UpgradeTableFactory;
 
 /**
  * @author Brian Wing Shun Chan
