be1fa91904a670af4d3e2e66702d17e4a3db94ec
Julius Lee <meng-huan.lee@liferay.com>
2023-10-06 18:01:00 -0700

    LPS-198458 Inline DigesterImpl into DigesterUtil

--- portal-kernel/src/com/liferay/portal/kernel/util/DigesterUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/DigesterUtil.java
@@ -5,9 +5,19 @@
 
 package com.liferay.portal.kernel.util;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 import java.nio.ByteBuffer;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author Brian Wing Shun Chan
@@ -17,97 +27,180 @@ import java.nio.ByteBuffer;
 public class DigesterUtil {
 
 	public static String digest(ByteBuffer byteBuffer) {
-		return _digester.digest(byteBuffer);
+		return digest(Digester.DEFAULT_ALGORITHM, byteBuffer);
 	}
 
 	public static String digest(InputStream inputStream) {
-		return _digester.digest(inputStream);
+		return digest(Digester.DEFAULT_ALGORITHM, inputStream);
 	}
 
 	public static String digest(String text) {
-		return _digester.digest(text);
+		return digest(Digester.DEFAULT_ALGORITHM, text);
 	}
 
 	public static String digest(String algorithm, ByteBuffer byteBuffer) {
-		return _digester.digest(algorithm, byteBuffer);
+		if (_BASE_64) {
+			return digestBase64(algorithm, byteBuffer);
+		}
+
+		return digestHex(algorithm, byteBuffer);
 	}
 
 	public static String digest(String algorithm, InputStream inputStream) {
-		return _digester.digest(algorithm, inputStream);
+		if (_BASE_64) {
+			return digestBase64(algorithm, inputStream);
+		}
+
+		return digestHex(algorithm, inputStream);
 	}
 
 	public static String digest(String algorithm, String... text) {
-		return _digester.digest(algorithm, text);
+		if (_BASE_64) {
+			return digestBase64(algorithm, text);
+		}
+
+		return digestHex(algorithm, text);
 	}
 
 	public static String digestBase64(ByteBuffer byteBuffer) {
-		return _digester.digestBase64(byteBuffer);
+		return digestBase64(Digester.DEFAULT_ALGORITHM, byteBuffer);
 	}
 
 	public static String digestBase64(InputStream inputStream) {
-		return _digester.digestBase64(inputStream);
+		return digestBase64(Digester.DEFAULT_ALGORITHM, inputStream);
 	}
 
 	public static String digestBase64(String text) {
-		return _digester.digestBase64(text);
+		return digestBase64(Digester.DEFAULT_ALGORITHM, text);
 	}
 
 	public static String digestBase64(String algorithm, ByteBuffer byteBuffer) {
-		return _digester.digestBase64(algorithm, byteBuffer);
+		byte[] bytes = digestRaw(algorithm, byteBuffer);
+
+		return Base64.encode(bytes);
 	}
 
 	public static String digestBase64(
 		String algorithm, InputStream inputStream) {
 
-		return _digester.digestBase64(algorithm, inputStream);
+		byte[] bytes = digestRaw(algorithm, inputStream);
+
+		return Base64.encode(bytes);
 	}
 
 	public static String digestBase64(String algorithm, String... text) {
-		return _digester.digestBase64(algorithm, text);
+		byte[] bytes = digestRaw(algorithm, text);
+
+		return Base64.encode(bytes);
 	}
 
 	public static String digestHex(ByteBuffer byteBuffer) {
-		return _digester.digestHex(byteBuffer);
+		return digestHex(Digester.DEFAULT_ALGORITHM, byteBuffer);
 	}
 
 	public static String digestHex(InputStream inputStream) {
-		return _digester.digestHex(inputStream);
+		return digestHex(Digester.DEFAULT_ALGORITHM, inputStream);
 	}
 
 	public static String digestHex(String text) {
-		return _digester.digestHex(text);
+		return digestHex(Digester.DEFAULT_ALGORITHM, text);
 	}
 
 	public static String digestHex(String algorithm, ByteBuffer byteBuffer) {
-		return _digester.digestHex(algorithm, byteBuffer);
+		byte[] bytes = digestRaw(algorithm, byteBuffer);
+
+		return StringUtil.bytesToHexString(bytes);
 	}
 
 	public static String digestHex(String algorithm, InputStream inputStream) {
-		return _digester.digestHex(algorithm, inputStream);
+		byte[] bytes = digestRaw(algorithm, inputStream);
+
+		return StringUtil.bytesToHexString(bytes);
 	}
 
 	public static String digestHex(String algorithm, String... text) {
-		return _digester.digestHex(algorithm, text);
+		byte[] bytes = digestRaw(algorithm, text);
+
+		return StringUtil.bytesToHexString(bytes);
 	}
 
 	public static byte[] digestRaw(ByteBuffer byteBuffer) {
-		return _digester.digestRaw(byteBuffer);
+		return digestRaw(Digester.DEFAULT_ALGORITHM, byteBuffer);
 	}
 
 	public static byte[] digestRaw(String text) {
-		return _digester.digestRaw(text);
+		return digestRaw(Digester.DEFAULT_ALGORITHM, text);
 	}
 
 	public static byte[] digestRaw(String algorithm, ByteBuffer byteBuffer) {
-		return _digester.digestRaw(algorithm, byteBuffer);
+		MessageDigest messageDigest = null;
+
+		try {
+			messageDigest = MessageDigest.getInstance(algorithm);
+
+			messageDigest.update(byteBuffer);
+		}
+		catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+			_log.error(noSuchAlgorithmException);
+		}
+
+		return messageDigest.digest();
 	}
 
-	public static byte[] digestRaw(String algorithm, InputStream inputStream) {
-		return _digester.digestRaw(algorithm, inputStream);
+	public static byte[] digestRaw(String algorithm, InputStream inputStream1) {
+		MessageDigest messageDigest = null;
+
+		try (InputStream inputStream2 = inputStream1) {
+			messageDigest = MessageDigest.getInstance(algorithm);
+
+			byte[] buffer = new byte[StreamUtil.BUFFER_SIZE];
+
+			int read = 0;
+
+			while ((read = inputStream2.read(buffer)) != -1) {
+				if (read > 0) {
+					messageDigest.update(buffer, 0, read);
+				}
+			}
+		}
+		catch (IOException ioException) {
+			_log.error(ioException);
+		}
+		catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+			_log.error(noSuchAlgorithmException);
+		}
+
+		return messageDigest.digest();
 	}
 
 	public static byte[] digestRaw(String algorithm, String... text) {
-		return _digester.digestRaw(algorithm, text);
+		MessageDigest messageDigest = null;
+
+		try {
+			messageDigest = MessageDigest.getInstance(algorithm);
+
+			StringBundler sb = new StringBundler((text.length * 2) - 1);
+
+			for (String t : text) {
+				if (sb.length() > 0) {
+					sb.append(StringPool.COLON);
+				}
+
+				sb.append(t);
+			}
+
+			String s = sb.toString();
+
+			messageDigest.update(s.getBytes(Digester.ENCODING));
+		}
+		catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+			_log.error(noSuchAlgorithmException);
+		}
+		catch (UnsupportedEncodingException unsupportedEncodingException) {
+			_log.error(unsupportedEncodingException);
+		}
+
+		return messageDigest.digest();
 	}
 
 	public static Digester getDigester() {
@@ -117,6 +210,14 @@ public class DigesterUtil {
 	public void setDigester(Digester digester) {
 		_digester = digester;
 	}
+
+	private static final boolean _BASE_64 = PropsUtil.get(
+		PropsKeys.PASSWORDS_DIGEST_ENCODING
+	).equals(
+		"base64"
+	);
+
+	private static final Log _log = LogFactoryUtil.getLog(DigesterUtil.class);
 
 	private static Digester _digester;
 
