e05cf6a4937e4c540a0eb61abf3c1a7509e9702a
Julius Lee <meng-huan.lee@liferay.com>
2023-10-09 18:23:12 -0700

    LPS-197376 Move HttpAuthManagerUtil to portal-impl
    # breaking_change_report
    
    ## What portal-impl/src/com/liferay/portal/security/auth/http.HttpAuthManagerUtil.java
    
    HttpAuthManagerUtil moving from portal-kernel into portal-impl.
    
    ## Why
    
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    
    ## Alternatives
    
    Add portal-impl in build dependency and use the same class.

--- portal-impl/src/com/liferay/portal/security/auth/http/HttpAuthManagerUtil.java
+++ portal-impl/src/com/liferay/portal/security/auth/http/HttpAuthManagerUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.security.auth.http;
+package com.liferay.portal.security.auth.http;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.module.service.Snapshot;
