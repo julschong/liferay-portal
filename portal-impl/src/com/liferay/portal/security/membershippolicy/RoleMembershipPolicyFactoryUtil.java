b3d94a87d23dbf808b9c6209812d53ae842d182f
Julius Lee <meng-huan.lee@liferay.com>
2023-10-17 13:30:25 -0700

    LPS-199389 Inline RoleMembershipPolicyFactoryImpl logic into Util

--- portal-impl/src/com/liferay/portal/security/membershippolicy/RoleMembershipPolicyFactoryUtil.java
+++ portal-impl/src/com/liferay/portal/security/membershippolicy/RoleMembershipPolicyFactoryUtil.java
@@ -5,8 +5,18 @@
 
 package com.liferay.portal.security.membershippolicy;
 
+import com.liferay.petra.concurrent.DCLSingleton;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.security.membershippolicy.RoleMembershipPolicy;
-import com.liferay.portal.kernel.security.membershippolicy.RoleMembershipPolicyFactory;
+import com.liferay.portal.util.PropsValues;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Roberto Díaz
@@ -14,19 +24,72 @@ import com.liferay.portal.kernel.security.membershippolicy.RoleMembershipPolicyF
 public class RoleMembershipPolicyFactoryUtil {
 
 	public static RoleMembershipPolicy getRoleMembershipPolicy() {
-		return _roleMembershipPolicyFactory.getRoleMembershipPolicy();
+		ServiceTracker<RoleMembershipPolicy, RoleMembershipPolicy>
+			serviceTracker = _serviceTrackerDCLSingleton.getSingleton(
+				RoleMembershipPolicyFactoryUtil::_createServiceTracker);
+
+		return serviceTracker.getService();
 	}
 
-	public static RoleMembershipPolicyFactory getRoleMembershipPolicyFactory() {
-		return _roleMembershipPolicyFactory;
+	private static ServiceTracker<RoleMembershipPolicy, RoleMembershipPolicy>
+		_createServiceTracker() {
+
+		ServiceTracker<RoleMembershipPolicy, RoleMembershipPolicy>
+			serviceTracker = new ServiceTracker<>(
+				_bundleContext, RoleMembershipPolicy.class,
+				new RoleMembershipPolicyTrackerCustomizer());
+
+		serviceTracker.open();
+
+		return serviceTracker;
 	}
 
-	public void setRoleMembershipPolicyFactory(
-		RoleMembershipPolicyFactory roleMembershipPolicyFactory) {
+	private static final Log _log = LogFactoryUtil.getLog(
+		RoleMembershipPolicyFactoryUtil.class);
 
-		_roleMembershipPolicyFactory = roleMembershipPolicyFactory;
+	private static final BundleContext _bundleContext =
+		SystemBundleUtil.getBundleContext();
+	private static final DCLSingleton
+		<ServiceTracker<RoleMembershipPolicy, RoleMembershipPolicy>>
+			_serviceTrackerDCLSingleton = new DCLSingleton<>();
+
+	private static class RoleMembershipPolicyTrackerCustomizer
+		implements ServiceTrackerCustomizer
+			<RoleMembershipPolicy, RoleMembershipPolicy> {
+
+		@Override
+		public RoleMembershipPolicy addingService(
+			ServiceReference<RoleMembershipPolicy> serviceReference) {
+
+			RoleMembershipPolicy roleMembershipPolicy =
+				_bundleContext.getService(serviceReference);
+
+			if (PropsValues.MEMBERSHIP_POLICY_AUTO_VERIFY) {
+				try {
+					roleMembershipPolicy.verifyPolicy();
+				}
+				catch (PortalException portalException) {
+					_log.error(portalException);
+				}
+			}
+
+			return roleMembershipPolicy;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<RoleMembershipPolicy> serviceReference,
+			RoleMembershipPolicy roleMembershipPolicy) {
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<RoleMembershipPolicy> serviceReference,
+			RoleMembershipPolicy roleMembershipPolicy) {
+
+			_bundleContext.ungetService(serviceReference);
+		}
+
 	}
-
-	private static RoleMembershipPolicyFactory _roleMembershipPolicyFactory;
 
 }
