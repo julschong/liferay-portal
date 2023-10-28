e59ca567fe265e8ccd2bff438dfba664f8928120
Julius Lee <meng-huan.lee@liferay.com>
2023-10-27 17:21:58 -0700

    LPS-195954 Move CacheKeyGeneratorUtil to portal-impl
    # breaking_change_report
    ## What portal-kernel/src/com/liferay/portal/kernel/cache/key/CacheKeyGeneratorUtil.java
    CacheKeyGeneratorUtil is moving from portal-kernel into portal-impl.
    ## Why
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    ## Alternatives
    Add portal-impl in build dependency and use the same class.
    ----

--- portal-impl/src/com/liferay/portal/cache/key/CacheKeyGeneratorUtil.java
+++ portal-impl/src/com/liferay/portal/cache/key/CacheKeyGeneratorUtil.java
@@ -1,10 +1,11 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.cache.key;
+package com.liferay.portal.cache.key;
 
+import com.liferay.portal.kernel.cache.key.CacheKeyGenerator;
 import com.liferay.portal.kernel.cache.thread.local.Lifecycle;
 import com.liferay.portal.kernel.cache.thread.local.ThreadLocalCache;
 import com.liferay.portal.kernel.cache.thread.local.ThreadLocalCacheManager;
