10c52c1cd94926df667ac8e1616058a5e993466d
Julius Lee <meng-huan.lee@liferay.com>
2023-10-31 14:21:08 -0700

    LPS-196625 Move MappingSqlQuery, MappingSqlQueryImpl, MappingSqlQueryFactoryUtil to portal-impl
    # breaking_change_report
    ## portal-kernel/src/com/liferay/portal/kernel/dao/jdbc/MappingSqlQuery.java
    MappingSqlQuery is moving from portal-kernel into portal-impl.
    ## Why
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    ## Alternatives
    Add portal-impl in build dependency and use the same class.
    ----
    
    # breaking_change_report
    ## What portal-kernel/src/com/liferay/portal/kernel/dao/jdbc/MappingSqlQueryImpl.java
    MappingSqlQueryImpl is moving from portal-kernel into portal-impl.
    ## Why
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    ## Alternatives
    Add portal-impl in build dependency and use the same class.
    ----
    
    # breaking_change_report
    ## What portal-kernel/src/com/liferay/portal/kernel/dao/jdbc/MappingSqlQueryFactoryUtil.java
    MappingSqlQueryFactoryUtil is moving from portal-kernel into portal-impl.
    ## Why
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    ## Alternatives
    Add portal-impl in build dependency and use the same class.
    ----

--- portal-impl/src/com/liferay/portal/dao/jdbc/MappingSqlQueryFactoryUtil.java
+++ portal-impl/src/com/liferay/portal/dao/jdbc/MappingSqlQueryFactoryUtil.java
@@ -3,7 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.dao.jdbc;
+package com.liferay.portal.dao.jdbc;
+
+import com.liferay.portal.kernel.dao.jdbc.ParamSetter;
+import com.liferay.portal.kernel.dao.jdbc.RowMapper;
 
 import javax.sql.DataSource;
 
