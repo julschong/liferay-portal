c8ddf709db0cfb5d11eec1c305ec731b5ce4052c
Julius Lee <meng-huan.lee@liferay.com>
2023-10-31 14:21:08 -0700

    LPS-XXXXXXX Move MappingSqlQuery, MappingSqlQueryImpl, MappingSqlQueryFactoryUtil to portal-impl

--- portal-impl/src/com/liferay/portal/dao/jdbc/MappingSqlQueryImpl.java
+++ portal-impl/src/com/liferay/portal/dao/jdbc/MappingSqlQueryImpl.java
@@ -3,9 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.dao.jdbc;
+package com.liferay.portal.dao.jdbc;
 
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.jdbc.ConnectionUtil;
+import com.liferay.portal.kernel.dao.jdbc.ParamSetter;
+import com.liferay.portal.kernel.dao.jdbc.RowMapper;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
