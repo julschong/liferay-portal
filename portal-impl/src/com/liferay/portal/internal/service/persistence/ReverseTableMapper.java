c1ee73cf1ede0ee741515d6966cd9ddf314f910c
Julius Lee <meng-huan.lee@liferay.com>
2023-10-31 14:06:32 -0700

    LPS-196625 Move TableMapperImpl, CTTableMapper, ReverseTableMapper to portal-impl

--- portal-impl/src/com/liferay/portal/internal/service/persistence/ReverseTableMapper.java
+++ portal-impl/src/com/liferay/portal/internal/service/persistence/ReverseTableMapper.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.internal.service.persistence;
+package com.liferay.portal.internal.service.persistence;
 
 import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.service.persistence.impl.TableMapper;
