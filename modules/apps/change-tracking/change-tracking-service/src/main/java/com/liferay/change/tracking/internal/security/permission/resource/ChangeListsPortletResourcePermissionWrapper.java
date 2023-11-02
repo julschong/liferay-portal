2dae618c8f973eaf945f51be69cc1fe767750919
Julius Lee <meng-huan.lee@liferay.com>
2023-11-02 15:00:11 -0700

    LPS-200702 Create ChangeListsPortletResourcePermissionWrapper

---
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/security/permission/resource/ChangeListsPortletResourcePermissionWrapper.java
@@ -0,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.change.tracking.internal.security.permission.resource;
+
+import com.liferay.portal.kernel.security.permission.resource.BasePortletResourcePermissionWrapper;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Preston Crary
+ * @author Julius Lee
+ */
+@Component(service = PortletResourcePermission.class)
+public class ChangeListsPortletResourcePermissionWrapper
+	extends BasePortletResourcePermissionWrapper {
+
+	@Override
+	protected PortletResourcePermission doGetPortletResourcePermission() {
+	}
+
+}
