cc03094d8355eeb41fa1dda6d65bf8c73d125f95
Julius Lee <meng-huan.lee@liferay.com>
2023-11-06 17:36:44 -0800

    LPS-200712 Move logic from Definition to Wrapper

--- modules/apps/commerce/commerce-pricing-service/src/main/java/com/liferay/commerce/pricing/internal/security/permission/resource/CommercePriceListPortletResourcePermissionWrapper.java
+++ modules/apps/commerce/commerce-pricing-service/src/main/java/com/liferay/commerce/pricing/internal/security/permission/resource/CommercePriceListPortletResourcePermissionWrapper.java
@@ -1,12 +1,14 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.commerce.pricing.internal.security.permission.resource;
 
+import com.liferay.commerce.price.list.constants.CommercePriceListConstants;
 import com.liferay.portal.kernel.security.permission.resource.BasePortletResourcePermissionWrapper;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -14,12 +16,18 @@ import org.osgi.service.component.annotations.Component;
  * @author Alessio Antonio Rendina
  * @author Julius Lee
  */
-@Component(service = PortletResourcePermission.class)
+@Component(
+	property = "resource.name=" + CommercePriceListConstants.RESOURCE_NAME,
+	service = PortletResourcePermission.class
+)
 public class CommercePriceListPortletResourcePermissionWrapper
 	extends BasePortletResourcePermissionWrapper {
 
 	@Override
 	protected PortletResourcePermission doGetPortletResourcePermission() {
+		return PortletResourcePermissionFactory.create(
+			CommercePriceListConstants.RESOURCE_NAME,
+			new PricingPortletResourcePermissionLogic());
 	}
 
 }
