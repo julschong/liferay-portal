add74988bb58ccaf2ae5e5047b19f68cb205819c
Julius Lee <meng-huan.lee@liferay.com>
2023-11-10 11:07:13 -0800

    LPS-195954 Apply to usages and remove MD5 unit test

--- portal-impl/test/unit/com/liferay/portal/cache/key/JavaSHA1CacheKeyGeneratorTest.java
+++ portal-impl/test/unit/com/liferay/portal/cache/key/JavaSHA1CacheKeyGeneratorTest.java
@@ -5,7 +5,6 @@
 
 package com.liferay.portal.cache.key;
 
-import com.liferay.portal.kernel.util.Digester;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import org.junit.Before;
@@ -26,7 +25,7 @@ public class JavaSHA1CacheKeyGeneratorTest
 	@Before
 	@Override
 	public void setUp() throws Exception {
-		cacheKeyGenerator = new MessageDigestCacheKeyGenerator(Digester.SHA_1);
+		cacheKeyGenerator = new MessageDigestCacheKeyGenerator();
 	}
 
 }
