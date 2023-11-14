5834e8dc2a7e512dcae96636239c4b4acaffdbfa
Julius Lee <meng-huan.lee@liferay.com>
2023-11-14 12:04:58 -0800

    LPS-201758 Move PortletResourcePermissionDefinitionTracker into portal-security-permission-impl

--- modules/apps/portal-security/portal-security-permission-impl/src/main/java/com/liferay/portal/security/permission/internal/resource/PortletResourcePermissionDefinitionTracker.java
+++ modules/apps/portal-security/portal-security-permission-impl/src/main/java/com/liferay/portal/security/permission/internal/resource/PortletResourcePermissionDefinitionTracker.java
@@ -3,8 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.internal.security.permission.resource;
+package com.liferay.portal.security.permission.internal.resource;
 
+import com.liferay.portal.kernel.internal.security.permission.resource.DefaultPortletResourcePermission;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.definition.PortletResourcePermissionDefinition;
