023a7877225411c87a23a9b565ef8d13267b2df5
Julius Lee <meng-huan.lee@liferay.com>
2023-11-17 14:30:47 -0800

    LPS-202077 Move ThemeResourceParserTest and ClassLoaderResourceParserTest to new module: portal-template-impl

--- modules/apps/portal-template/portal-template-impl/src/test/java/com/liferay/portal/template/internal/resource/parser/ClassLoaderResourceParserTest.java
+++ modules/apps/portal-template/portal-template-impl/src/test/java/com/liferay/portal/template/internal/resource/parser/ClassLoaderResourceParserTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.template;
+package com.liferay.portal.template.internal.resource.parser;
 
 import com.liferay.petra.lang.ClassLoaderPool;
 import com.liferay.petra.string.StringBundler;
--- modules/apps/portal-template/portal-template-impl/src/test/java/com/liferay/portal/template/internal/resource/parser/TemplatePortletPreferencesTest.java
+++ modules/apps/portal-template/portal-template-impl/src/test/java/com/liferay/portal/template/internal/resource/parser/TemplatePortletPreferencesTest.java
@@ -3,12 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.template;
+package com.liferay.portal.template.internal.resource.parser;
 
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.portal.kernel.security.xml.SecureXMLFactoryProviderUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.security.xml.SecureXMLFactoryProviderImpl;
+import com.liferay.portal.template.TemplatePortletPreferences;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.portal.tools.ToolDependencies;
 import com.liferay.portlet.PortletPreferencesFactoryImpl;
