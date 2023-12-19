902efa7c56a5c1ff2a21c1f2d61ee10e93df2661
Julius Lee <meng-huan.lee@liferay.com>
2023-12-19 11:27:14 -0800

    LPS-204357 Move LayoutSetPrototypeLayoutModelListener into layout-set-prototype-web and convert into OSGi component

--- modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/model/listener/LayoutSetPrototypeLayoutModelListener.java
+++ modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/model/listener/LayoutSetPrototypeLayoutModelListener.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.service.impl;
+package com.liferay.layout.set.prototype.web.internal.model.listener;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -12,15 +12,19 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
+import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutSetLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutSetPrototypeLocalServiceUtil;
 
 import java.util.Date;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Raymond Augé
  */
+@Component(service = ModelListener.class)
 public class LayoutSetPrototypeLayoutModelListener
 	extends BaseModelListener<Layout> {
 
