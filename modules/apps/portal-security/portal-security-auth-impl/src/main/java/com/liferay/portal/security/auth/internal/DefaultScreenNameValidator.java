44f06e45783629440c0a7b1e512d2c53bea3aa60
Julius Lee <meng-huan.lee@liferay.com>
2024-01-12 17:05:35 -0800

    LPS-X Move DefaultScreenNameValidator and LiberalScreenNameValidator to modules
    # breaking
    
    ## What portal-kernel/src/com/liferay/portal/kernel/security/auth/DefaultScreenNameValidator.java
    
    DefaultScreenNameValidator is moving to modules.
    
    ## Why
    
    We are eliminating all the Spring beans to be able to merge portal-kernel and portal-impl
    
    ----

--- modules/apps/portal-security/portal-security-auth-impl/src/main/java/com/liferay/portal/security/auth/internal/DefaultScreenNameValidator.java
+++ modules/apps/portal-security/portal-security-auth-impl/src/main/java/com/liferay/portal/security/auth/internal/DefaultScreenNameValidator.java
@@ -3,10 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.security.auth;
+package com.liferay.portal.security.auth.internal;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.security.auth.ScreenNameValidator;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
--- modules/apps/portal-security/portal-security-auth-impl/src/test/java/com/liferay/portal/security/auth/internal/DefaultScreenNameValidatorTest.java
+++ modules/apps/portal-security/portal-security-auth-impl/src/test/java/com/liferay/portal/security/auth/internal/DefaultScreenNameValidatorTest.java
@@ -3,10 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.security.auth;
+package com.liferay.portal.security.auth.internal;
 
 import com.liferay.petra.string.CharPool;
-import com.liferay.portal.kernel.security.auth.DefaultScreenNameValidator;
 import com.liferay.portal.kernel.test.util.PropsTestUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
