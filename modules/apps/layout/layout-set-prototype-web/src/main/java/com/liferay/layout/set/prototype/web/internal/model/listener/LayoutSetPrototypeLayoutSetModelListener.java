72b04aa4462761bc1ad0edb69c81956992a034d9
Julius Lee <meng-huan.lee@liferay.com>
2024-01-19 17:56:31 -0800

    LPS-X Move LayoutSetPrototypeLayoutSetModelListener to module: layout-set-prototype-web

--- modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/model/listener/LayoutSetPrototypeLayoutSetModelListener.java
+++ modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/model/listener/LayoutSetPrototypeLayoutSetModelListener.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.service.impl;
+package com.liferay.layout.set.prototype.web.internal.model.listener;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
